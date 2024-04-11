3c7536a4eacb1b007dbfac8c85bccd5b6c25798e
Connor Morin <cjconnor@email.unc.edu>
2024-04-11 15:06:24 -0400

    Trying login/logout fix

--- web/omlweb/urls.py
+++ web/omlweb/urls.py
@@ -6,13 +6,17 @@ from omlweb.views import home, summary, billing, results, billPDF, \
 from django.contrib import admin
 admin.autodiscover()
 
-from django.contrib.auth.views import login, logout
+#from django.contrib.auth.views import login, logout
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     re_path(r'^$', home),
     
-    re_path(r'^login/$', login, {'template_name': 'login.html'}),
-    re_path(r'^logout/$', logout, {'next_page': '/login'}),
+    #re_path(r'^login/$', login, {'template_name': 'login.html'}),
+    #re_path(r'^logout/$', logout, {'next_page': '/login'}),
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(next_page='/login'), name='logout'),
     
     re_path(r'^summary/$', summary),
     re_path(r'^billing/$', billing),
@@ -27,5 +31,5 @@ urlpatterns = [
     re_path(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    re_path(r'^admin/', include(admin.site.urls)),
+    re_path(r'^admin/', admin.site.urls),
 ]
