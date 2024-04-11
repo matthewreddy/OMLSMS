82b3bc905b8117bf40f975e5073e16d9ace7250f
Connor Morin <cjconnor@email.unc.edu>
2024-04-11 14:49:18 -0400

    Attempted url fix

--- web/omlweb/urls.py
+++ web/omlweb/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.urls import include, re_path
 from omlweb.views import home, summary, billing, results, billPDF, \
                             resultsPDF, invalidSterilizer
 
@@ -8,24 +8,24 @@ admin.autodiscover()
 
 from django.contrib.auth.views import login, logout
 
-urlpatterns = patterns('',
-    url(r'^$', home),
+urlpatterns = [
+    re_path(r'^$', home),
     
-    url(r'^login/$', login, {'template_name': 'login.html'}),
-    url(r'^logout/$', logout, {'next_page': '/login'}),
+    re_path(r'^login/$', login, {'template_name': 'login.html'}),
+    re_path(r'^logout/$', logout, {'next_page': '/login'}),
     
-    url(r'^summary/$', summary),
-    url(r'^billing/$', billing),
-    url(r'^results/$', results),
+    re_path(r'^summary/$', summary),
+    re_path(r'^billing/$', billing),
+    re_path(r'^results/$', results),
 
-    url(r'^billing/print_bill/(\d{3,5})$', billPDF),
-    url(r'^results/print_results/(\d{3,5})$', resultsPDF),
+    re_path(r'^billing/print_bill/(\d{3,5})$', billPDF),
+    re_path(r'^results/print_results/(\d{3,5})$', resultsPDF),
     
-    url(r'^invalid/sterilizer_id/(\d{3,5})$', invalidSterilizer),
+    re_path(r'^invalid/sterilizer_id/(\d{3,5})$', invalidSterilizer),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    re_path(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
+    re_path(r'^admin/', include(admin.site.urls)),
+]
